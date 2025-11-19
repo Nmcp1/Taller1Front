69f1c6511d74cf69ea28f4504c841c11ed40c54b
Nmcp1 <141276162+Nmcp1@users.noreply.github.com>
2025-11-19 19:21:56 -0300

    base

---
+++ Taller1Front/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className="row">
+      <h1>Base Certamen2</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
